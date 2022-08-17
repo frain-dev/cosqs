09b2374e9a6006c2d52c4385e60323a7c878c0a1
Dotun Jolaoso <dotunjolaosho@gmail.com>
2022-08-17 16:15:51 +0100

    add support for discovery process

--- cmd/ingest.go
+++ cmd/ingest.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"sync"
 	"time"
-	_ "time"
 
 	"github.com/frain-dev/cosqs/mongo"
 	"github.com/spf13/cobra"
@@ -17,20 +16,10 @@ import (
 	"github.com/dchest/uniuri"
 )
 
-type Cosqs struct {
-	message  *sqs.Message
-	queueUrl string
-	id       string
-	sqs      *sqs.SQS
-}
-
 var (
-	chanMessages    = make(chan *Cosqs)
-	chanDelMessages = make(chan *Cosqs)
-	sfc             = &SafeCounter{wc: map[string]int{}}
+	sfc          = &SafeCounter{wc: make(map[string]int)}
+	sourceStream = make(map[string]mongo.Source)
 )
-
-// map {source_id: 9}
 
 /**
 1. How do we scale the ingesters?
@@ -46,36 +35,15 @@ func addIngesterCommand(a *application) *cobra.Command {
 		Use:   "ingest",
 		Short: "convoy ingester",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			sources, err := a.sourceRepo.LoadSources(context.Background())
-			if err != nil {
-				return err
-			}
 
-			for _, source := range sources {
-				var wg sync.WaitGroup
+			ticker := time.NewTicker(1 * time.Minute)
 
-				//multiple clients
-				//multiple threads per client
+			for range ticker.C {
+				err := discoverSources(a.sourceRepo)
 
-				if source.Workers > 1 {
-					wg.Add(source.Workers)
-					for i := 1; i <= source.Workers; i++ {
-						go setupQueueListener(source, &wg)
-					}
-
-					wg.Wait()
-				} else {
-					fmt.Println("here2")
-					go setupQueueListener(source, nil)
+				if err != nil {
+					fmt.Println("error with fetching sources,", err)
 				}
-			}
-
-			// ticker := time.NewTicker(1 * time.Second)
-
-			for message := range chanMessages {
-				handleMessage(message)
-				deleteMessage(message)
-				// emitMessage(message)
 			}
 
 			return nil
@@ -85,9 +53,36 @@ func addIngesterCommand(a *application) *cobra.Command {
 	return cmd
 }
 
+func discoverSources(sourceRepo mongo.SourceRepository) error {
+	sources, err := sourceRepo.LoadSources(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, source := range sources {
+		_, exists := sourceStream[source.UID]
+
+		// We've seen this source already
+		if exists {
+			continue
+		}
+
+		dispatchSource(source)
+		sourceStream[source.UID] = source
+
+	}
+
+	return nil
+}
+
+func dispatchSource(source mongo.Source) {
+	for i := 1; i <= source.Workers; i++ {
+		go setupQueueListener(source)
+	}
+}
+
 //Sets up a new queue listener
-func setupQueueListener(source mongo.Source, wg *sync.WaitGroup) {
-	// fmt.Printf("Setting up queue listener for source with ID :%s\n", source.UID)
+func setupQueueListener(source mongo.Source) {
 	id := uniuri.NewLen(6)
 	fmt.Printf("Running worker with ID: %s\n", id)
 	sess, err := session.NewSession(&aws.Config{
@@ -117,36 +112,23 @@ func setupQueueListener(source mongo.Source, wg *sync.WaitGroup) {
 		fmt.Printf("failed to fetch sqs message %v", err)
 	}
 
-	if wg != nil {
-		wg.Done()
-	}
-
-	// for i := 1; i <= source.Workers; i++ {
-	// 	go handle(id, chanMessages, chanDelMessages)
-	// }
-
-	// go handle(id, chanMessages, chanDelMessages)
-
 	//infinite loop to poll messages from the queue
 	for {
-		// fmt.Println("here for loop")
 		output, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
 			QueueUrl:            queueURL,
 			MaxNumberOfMessages: aws.Int64(10),
 			WaitTimeSeconds:     aws.Int64(1),
 		})
 
-		// fmt.Printf("worker UID: %s\n, messages length: %v\n, received time: %v\n", id, len(output.Messages), time.Now().Format("Jan _2 15:04:05.000000"))
-
 		if err != nil {
 			fmt.Printf("failed to fetch sqs message %v", err)
 		}
 
-		var wgg sync.WaitGroup
+		var wg sync.WaitGroup
 		for _, message := range output.Messages {
-			wgg.Add(1)
+			wg.Add(1)
 			go func(m *sqs.Message) {
-				defer wgg.Done()
+				defer wg.Done()
 				fmt.Println("RECEIVING MESSAGE >>>")
 				fmt.Println(*m.Body)
 				sfc.mu.Lock()
@@ -158,56 +140,9 @@ func setupQueueListener(source mongo.Source, wg *sync.WaitGroup) {
 					QueueUrl:      queueURL,
 					ReceiptHandle: m.ReceiptHandle,
 				})
-				// chanMessages <- &Cosqs{message: m, queueUrl: *queueURL, sqs: svc, id: id}
 			}(message)
 
-			// go func(m *sqs.Message) {
-			// }(message)
-
-			wgg.Wait()
-			// chanMessages <- &Cosqs{message: message, queueUrl: *queueURL, sqs: svc, id: id}
+			wg.Wait()
 		}
 	}
-}
-
-func handle(id string, cm <-chan *Cosqs, cdm chan<- *Cosqs) {
-	for m := range cm {
-		// fmt.Println("here with worker", m.id)
-		// fmt.Println("RECEIVING MESSAGE >>>")
-		// fmt.Println(*m.message.Body)
-		sfc.mu.Lock()
-		// sfc.count++
-		sfc.wc[m.id]++
-		exists, ok := sfc.wc[m.id]
-
-		if ok {
-			fmt.Printf("Worker UID: %v total number of messages received:%v\n", m.id, exists)
-
-		}
-
-		sfc.mu.Unlock()
-		cdm <- m
-	}
-}
-
-func handleMessage(data *Cosqs) {
-	// fmt.Println(*data.message.Body)
-	fmt.Printf("processed by worker with ID:%s\v :time: %v\n", data.id, time.Now().Format("Jan _2 15:04:05.000000"))
-
-}
-
-func emitMessage(data *Cosqs) {
-	sfc.mu.Lock()
-	sfc.wc[data.id]++
-	sfc.mu.Unlock()
-	fmt.Printf("map: %v\n, time: %v\n", sfc.wc, time.Now())
-
-}
-
-// delete message from queue
-func deleteMessage(data *Cosqs) {
-	data.sqs.DeleteMessage(&sqs.DeleteMessageInput{
-		QueueUrl:      &data.queueUrl,
-		ReceiptHandle: data.message.ReceiptHandle,
-	})
 }
